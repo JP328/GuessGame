d81b1599a3bd80bd55642ccce59c81f640c414b3
João Pedro <joaopedrodamaiamoura@outlook.com>
2022-06-02 22:26:22 -0300

    :sparkles: Updating 0.2 :sparkles:

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-const nick = document.querySelector('#nick')
+//--------Text--------//
+
 const message = document.querySelector('#message')
-const intervalo = document.querySelector('#intervalo')
 const advice = document.querySelector('#advice')
 const chances = document.querySelector('#chances')
+const winner = document.querySelector('#winner')
+const feedback = document.querySelector('#feedback')
+
+//--------Inputs--------//
+
+const intervalo = document.querySelector('#intervalo')
+const nick = document.querySelector('#nick')
 const guess = document.querySelector('#guess')
 
+//--------Open and Close PopUp--------//
+const togglePopUp = () => {
+  document.querySelector('.popUpMessage').classList.toggle('animation')
+  document.querySelector('.popUp').classList.toggle('active')
+}
+
+//--------Js Variable--------//
 let GuessNumber
 let chancesNumber = 2
 
-const resetGame = () => {
-  console.log(GuessNumber)
-  if (GuessNumber != undefined) {
-    chancesNumber = 2
-    chances.textContent = ''
-    advice.textContent = ''
-    message.textContent = ''
-    guess.value = ''
-    GuessNumber = undefined
-    console.log(GuessNumber)
-  }
-}
-
+//--------Js Functions--------//
 const startGame = () => {
   if (nick.value == '') {
     alert('Por favor, preencha um nome valido')
     throw new Error('Por favor, preencha um nome valido')
   } else {
     message.innerHTML = `Olá <strong>${nick.value}</strong>, vamos jogar!<br/>
-    De acordo com a opçao de intervalo que você escolheu, descubra o número.`
+    De acordo com a opção de intervalo que você escolheu, descubra o número.`
+    disabledToggle()
   }
   GuessNumber === undefined
     ? (GuessNumber = Math.round(Math.random() * intervalo.value))
@@ -35,28 +38,46 @@ const startGame = () => {
 }
 
 const tryGuess = () => {
-  let situation
-  // GuessNumber === undefined
-  //   ? (GuessNumber = Math.round(Math.random() * intervalo.value))
-  //   : ''
-
   console.log(GuessNumber)
+  guess.value == GuessNumber ? winnerPopUp() : numberOfTry()
+}
 
-  const logic =
-    guess.value > GuessNumber
-      ? (situation = 'O número é maior!')
-      : (situation = 'O número é menor')
-
-  guess.value == GuessNumber
-    ? (situation = 'Parabéns, você conseguiu adivinhar!')
-    : logic
-
-  advice.innerHTML = situation
+const winnerPopUp = () => {
+  togglePopUp()
+  winner.innerHTML = `<strong>Parabéns ${nick.value}, Você conseguiu adivinhar!<strong/>`
+  feedback.textContent = `Continue jogando, explore os modos mais difíceis e não pare até ficar craque nesse jogo!`
+  resetGame()
 }
 
 function numberOfTry() {
+  let situation
+  guess.value > GuessNumber
+    ? (situation = 'O número é maior!')
+    : (situation = 'O número é menor')
+
+  advice.innerHTML = situation
+
   chancesNumber > 0
-    ? (chances.innerHTML = `Você ainda tem ${chancesNumber} tentativas!`)
-    : (chances.innerHTML = 'Suas tentativas acabaram :(')
-  chancesNumber = chancesNumber - 1
+    ? (chances.textContent = `Você ainda tem ${chancesNumber} tentativas!`)
+    : (chances.textContent = 'Suas tentativas acabaram :(')
+
+  chancesNumber -= 1
+}
+
+const resetGame = () => {
+  if (GuessNumber != undefined) {
+    chancesNumber = 2
+    chances.textContent = ''
+    advice.textContent = ''
+    message.textContent = ''
+    guess.value = ''
+    GuessNumber = undefined
+    disabledToggle()
+  }
+}
+
+const disabledToggle = () => {
+  guess.toggleAttribute('disabled')
+  intervalo.toggleAttribute('disabled')
+  nick.toggleAttribute('disabled')
 }
